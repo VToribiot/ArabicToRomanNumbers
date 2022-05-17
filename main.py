945710a1a9ee65f5c31fd4ac14cde1c651d201d4
VToribiot <80557014+VToribiot@users.noreply.github.com>
2022-05-17 18:10:31 -0400

    conversion function

--- main.py
+++ main.py
@@ -12,6 +12,33 @@ def validation(s):
         return False
     return True
 
+def intToRom(n):
+
+    rules = (
+        ("M", 1000),
+        ("CM", 900),
+        ("D", 500),
+        ("CD", 400),
+        ("C", 100),
+        ("XC", 90),
+        ("L", 50),
+        ("XL", 40),
+        ("X", 10),
+        ("IX", 9),
+        ("V", 5),
+        ("IV", 4),
+        ("I", 1)
+    )
+
+    num = ""
+
+    for let, val in rules:  # Travels the entire tuple of tuples
+        while n >= val:  # Runs while the parameter is bigger that the current checked value
+            n -= val  # Subtracts that value to the parameter
+            num += let  # Adds the letter equivalent to the subtracted number
+
+    return num
+
 def main():
     print("Welcome to the Arabic Number to Roman Number Converter Program\n")
 
